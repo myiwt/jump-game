752503a6fefa421e284a0d2728af4274f2d1b3be
M <4558199+pajamamike@users.noreply.github.com>
2020-02-07 23:59:09 +1300

    Improvement of image drawing lag issues. Updated obstacle generation so that spawned obstacles have adequate spacing between them.

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
 # To run this game, open a command prompt terminal, type in: cd <the folder path this script is in>. Then type in the command :pgzrun game.py
 import pgzrun
-import time
 import random
 
 # ---GLOBAL GAME VARIABLES--- #
@@ -8,30 +7,26 @@ WIDTH = 800
 HEIGHT = 450
 TILESIZE = images.dirt.get_height()
 
+def remove_alpha(image):
+    return image.convert_alpha()
+
 class Background():
     images = {"background": images.background,
-              "dirt" : images.dirt,
-              "grass" : images.grassbackground}
+              "dirt" : images.dirt}
 
-    def __init__(self):
+    def __init__(self, speed):
         self.dirt_height = self.dirt_width = self.grass_height = TILESIZE
-        self.grass_width = self.images["grass"].get_width()
+        self.backgroundimage = remove_alpha(self.images["background"])
         self.background_x = 0
-        self.background2_x = self.images["background"].get_width()
-        self.grass_x = 0
-        self.grass2_x = self.images["grass"].get_width()
-        self.grass_y = HEIGHT - TILESIZE
+        self.background2_x = self.backgroundimage.get_width()
+        self.speed = speed
 
     def draw(self):
-        screen.blit(self.images["background"], (self.background_x, 0))
-        screen.blit(self.images["background"], (self.background2_x, 0))
-        screen.blit(self.images["grass"], (self.grass_x, self.grass_y))
-        screen.blit(self.images["grass"], (self.grass2_x, self.grass_y))
+        screen.blit(self.backgroundimage, (self.background_x, 0))
+        screen.blit(self.backgroundimage, (self.background2_x, 0))
 
-        self.background_x -= 5
-        self.background2_x -= 5
-        self.grass_x -= 5
-        self.grass2_x -= 5
+        self.background_x -= self.speed
+        self.background2_x -= self.speed
 
         if self.background_x < self.images["background"].get_width() * -1:
             self.background_x = self.images["background"].get_width()
@@ -39,17 +34,12 @@ class Background():
         if self.background2_x < self.images["background"].get_width() * -1:
             self.background2_x = self.images["background"].get_width()
 
-        if self.grass_x < self.images["grass"].get_width() * -1:
-            self.grass_x = self.images["grass"].get_width()
-
-        if self.grass2_x < self.images["grass"].get_width() * -1:
-            self.grass2_x = self.images["grass"].get_width() - 2
-
 class GameState():
-    def __init__(self):
+    def __init__(self, speed = 7):
         self.game_over = False
         self.score = 0
         self.player_hit = False
+        self.speed = speed
 
     def make_invulnerable(self):
         self.player_hit = False
@@ -61,14 +51,12 @@ class GameState():
         clock.schedule_unique(self.make_invulnerable,1.0)
 
 class Character():
-    player_images = {"RUN":
-                         [images.player0, images.player1, images.player2, images.player3,
-                          images.player4, images.player5, images.player6, images.player7,
-                          images.player8, images.player9, images.player10, images.player11,
-                          images.player12, images.player13, images.player14, images.player15,
-                          images.player16, images.player17],
-                     "JUMP":
-                         [images.jump0, images.jump1]}
+    player_images_with_alpha = {"RUN": [images.player0, images.player1, images.player2, images.player3, images.player4,
+                                        images.player5, images.player6, images.player7, images.player8],
+                                "JUMP": [images.jump0, images.jump1]}
+
+    player_images = {"RUN": [remove_alpha(image) for image in player_images_with_alpha["RUN"]],
+                     "JUMP": [remove_alpha(image) for image in player_images_with_alpha["JUMP"]]}
 
     def __init__(self):
         self.player_x = TILESIZE * 2
@@ -89,8 +77,7 @@ class Character():
             image_to_draw = self.player_images["RUN"][self.player_frame]
             screen.blit(image_to_draw, (self.player_x, self.player_y))
             self.hitbox = Rect((self.player_x, self.player_y), (image_to_draw.get_width(), image_to_draw.get_height()))
-            time.sleep(0.03)
-            if self.player_frame == 17:
+            if self.player_frame == len(self.player_images["RUN"])-1:
                 self.player_frame = 1
 
         elif self.jump:
@@ -112,11 +99,13 @@ class Character():
         screen.draw.rect(self.hitbox, color="RED")
 
 class ObstacleGeneration():
-    def __init__(self):
+    def __init__(self, player):
         self.level = 1
         self.empty_map_width = [0] * WIDTH
         self.object_id = {1: 'spike'}
         self.obstacle_list = []
+        self.player = player
+        self.obstacle_x_buffer = player.width + 10
 
     def map_width_generator(self, probability):
         map_width = self.empty_map_width
@@ -124,27 +113,40 @@ class ObstacleGeneration():
             rand = random.random()
             if rand < probability:
                 map_width[pixel[0]] = 1
+        for pixel in enumerate(map_width):
+             if pixel[0] > len(map_width) - self.obstacle_x_buffer:
+                 break
+             if pixel[1] == 1:
+                 for n in range(pixel[0]+ 1, pixel[0]+ self.obstacle_x_buffer):
+                     map_width[n] = 0
+        print(map_width)
         return map_width
 
     def obstacale_generator(self,objectid,x):
-        #[objectid, x, y]
-        self.obstacle_list.append([objectid,x,HEIGHT - TILESIZE - 15])
+        self.obstacle_list.append([objectid,x,HEIGHT - TILESIZE - 15]) #[objectid, x, y]
+
+    def game_map_generator(self,probability,frames):
+        game_map = self.empty_map_width
+        for frame in range(0,frames):
+            game_map.append(self.map_width_generator(probability = probability))
+        return game_map
 
 class Spike(object):
     image = images.spike
-    def __init__(self, x = WIDTH,hitbox = Rect(0,0,0,0)):
+    def __init__(self, speed,x = WIDTH,hitbox = Rect(0,0,0,0)):
         self.x = x
         self.hitbox = hitbox
         self.y = HEIGHT - TILESIZE - 15
         self.width = self.image.get_width()
         self.height = self.image.get_height()
         self.hit_once = False
+        self.speed = speed
 
     def draw(self):
         screen.blit(self.image,(self.x,self.y))
         self.hitbox = Rect((self.x, self.y), (self.width, self.height))
         screen.draw.rect(self.hitbox, color="RED")
-        self.x -= 5
+        self.x -= self.speed
         return self.hitbox
 
     def collide(self,rect):
@@ -153,14 +155,16 @@ class Spike(object):
         return False
 
 game = GameState()
+speed = game.speed
 player = Character()
-background = Background()
-obstaclegeneration = ObstacleGeneration()
-map = obstaclegeneration.map_width_generator(0.005)
+background = Background(speed=speed)
+obstaclegeneration = ObstacleGeneration(player = player)
+#map = obstaclegeneration.map_width_generator(probability=0.005)
+map = obstaclegeneration.game_map_generator(probability= 0.005, frames= 5)
 obj_list = []
 for pixel in enumerate(map):
     if pixel[1] == 1:
-        obj_list.append(Spike(x=pixel[0]))
+        obj_list.append(Spike(speed = speed, x=pixel[0]))
 
 def draw():
     background.draw()
